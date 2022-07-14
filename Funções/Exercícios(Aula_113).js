8a22a974fce3408aed3086cdcb1a9548bacd3eed
Davner Firmino Cavalcante <davner23@gmail.com>
2022-07-14 18:41:58 -0400

    Aulas - Comparação entre números e strings + resolução da questão 1 da lista de exercícios da aula 113 - 14/07/2022

---
+++ Funções/Exercícios(Aula_113).js
@@ -0,0 +1,11 @@
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                   Questão 01                                   ||
+// ! ||--------------------------------------------------------------------------------||
+function calculadoraSimples(a, b) {
+ console.log(`Soma = ${a + b}`);
+ console.log(`Subtração = ${a - b}`);
+ console.log(`Multiplicação = ${a * b}`);
+ console.log(`Divisão = ${Math.floor(a / b)}`);
+}
+
+calculadoraSimples(2, 234);
