e55b8bd1abc50a5b98ee9d706db5d31eaf0f6591
Davner Firmino Cavalcante <davner23@gmail.com>
2022-07-15 16:20:44 -0400

    Resolução da questão 2 da aula 113 - 15/07/2022

--- Funções/Exercícios(Aula_113).js
+++ Funções/Exercícios(Aula_113).js
@@ -9,3 +9,22 @@ function calculadoraSimples(a, b) {
 }
 
 calculadoraSimples(2, 234);
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                   Questão 02                                   ||
+// ! ||--------------------------------------------------------------------------------||
+function tresTirangulos(AB, BC, AC) {
+ if (AB != BC && AB != AC) {
+  console.log("Triângulo Escaleno");
+ } else if (AB == BC && AB != AC) {
+  console.log("Triângulo Isósceles");
+ } else if (AB == BC && AB == AC) {
+  console.log("Triângulo Equilátero");
+ } else {
+  console.log("Valores inválidos");
+ }
+}
+
+tresTirangulos(12, 14, 34);
+tresTirangulos(12, 12, 34);
+tresTirangulos(34, 34, 34);
