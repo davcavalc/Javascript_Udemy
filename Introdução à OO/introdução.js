348945e53a64413e200a5b5e9752188d6b90f52e
Davner Firmino Cavalcante <davner23@gmail.com>
2022-07-21 17:17:30 -0400

    📓Aula: Introdução à Orientação a Objetos (parte 1) - 21/07/2022

---
+++ Introdução à OO/introdução.js
@@ -0,0 +1,41 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// procedural
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+ valor1,
+ valor2,
+ valor3,
+
+ processamento() {
+  // ...
+ },
+};
+
+objeto.processamento();
+
+/**
+ * PRINCÍPIOS IMPORTANTES
+ * 1) Abstração
+ * 2) Encapsulamento
+ */
+
+class Pessoa {
+ constructor(nome, idade, sexo, dataDeNascimento) {
+  this.idade = idade;
+  this.nome = nome;
+  this.sexo = sexo;
+  this.dataDeNascimento = dataDeNascimento;
+ }
+
+ apresentar() {
+  console.log(
+   `Olá, meu nome é ${this.nome}, tenho ${this.idade} anos, ${this.sexo} e minha data de nascimento é ${this.dataDeNascimento}.`
+  );
+ }
+}
+
+let person = new Pessoa("Fulano", 20, "masculino", "21/07/1989");
+person.apresentar();
