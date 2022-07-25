7d8e38c15537538f7a67506b59c17724eea492cc
Davner Firmino Cavalcante <davner23@gmail.com>
2022-07-25 15:49:46 -0400

    📓Aula: Notação Literal (parte 4) - 25/07/2022

---
+++ Introdução à OO/NotaçãoLiteral.js
@@ -0,0 +1,29 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+const obj1 = { a: a, b: b, c: c };
+const obj2 = { a, b, c };
+console.log(obj1);
+console.log(obj2);
+
+const nomeAtr = "nota";
+const valorAtr = 7.87;
+
+const obj3 = {};
+obj3[nomeAtr] = valorAtr;
+console.log(obj3);
+
+const obj4 = { [nomeAtr]: valorAtr };
+console.log(obj4);
+
+const obj5 = {
+ function1: function () {
+  // ... forma tradicional
+ },
+ funcao2() {
+  // ... forma reduzida
+ },
+};
+
+console.log(obj5);
