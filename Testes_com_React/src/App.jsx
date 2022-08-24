760d8070e2e18872657d6aee18f2524101f45c26
Davner Firmino Cavalcante <davner23@gmail.com>
2022-08-24 16:46:26 -0400

    🧱Feat: Testes com Bulma CSS - 24/08/2022

--- Testes_com_React/src/App.jsx
+++ Testes_com_React/src/App.jsx
@@ -10,6 +10,12 @@ function App() {
 		<>
 			{/* <h1 className="text-center text-3xl text-bold mb-6">Renderização de listas</h1> */}
 			{/* <Lista items={link}/> */}
+			<section className="section">
+				<h1 className="title">Hello World</h1>
+				<p className="subtitle">My React app with Bulma CSS</p>
+				<p className="text-5xl font-bold">OK</p>
+				<button className="button is-danger mt-4 rounded-full">Click here</button>
+			</section>
 			<LinkTree />
 		</>
 	);
